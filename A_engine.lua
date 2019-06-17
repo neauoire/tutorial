466f2b486f1f49fe7d44970817135c7f67fc280b
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-17 16:10:26 +0900

    Starting engine

---
+++ A_engine.lua
@@ -0,0 +1,52 @@
+-- A
+-- B
+-- C
+-- 
+
+local viewport = { width = 128, height = 64, frame = 0 }
+
+-- Main
+
+function init()
+  -- Render Style
+  screen.level(15)
+  screen.aa(0)
+  screen.line_width(1)
+  -- Render
+  update()
+end
+
+function update()
+  redraw()
+end
+
+-- Interactions
+
+function key(id,state)
+  update()
+end
+
+function enc(id,delta)
+  update()
+end
+
+-- Render
+
+function draw_frame()
+  screen.level(10)
+  screen.rect(1, 1, viewport.width-1, viewport.height-1)
+  screen.stroke()
+end
+
+function redraw()
+  screen.clear()
+  draw_frame()
+  screen.stroke()
+  screen.update()
+end
+
+-- Utils
+
+function clamp(val,min,max)
+  return val < min and min or val > max and max or val
+end

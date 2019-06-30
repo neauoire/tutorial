9d22a5a8fe1ae2e70816b65297443f543180d0e4
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-30 07:51:21 +0700

    Improved params with library example

--- B_parameters.lua
+++ B_parameters.lua
@@ -7,9 +7,9 @@
 --   \\\\////
 --
 
+local mdh = include('lib/midi_device_helper')
+
 local viewport = { width = 128, height = 64 }
-local focus = { x = 0, y = 0 }
-local options = {"Cyan", "Magenta", "Yellow", "Black"}
 local actions = {"Yes", "No"}
 
 -- Main
@@ -19,14 +19,13 @@ function init()
   screen.level(15)
   screen.aa(0)
   screen.line_width(1)
-  -- Center focus
-  focus.x = viewport.width/2
-  focus.y = viewport.height/2
   -- Render
   redraw()
+  -- Add params with a library
+  mdh:init()
+  -- Script Params
   params:add{type = "number", id = "number", name = "Number", min = 1, max = 48, default = 4}
-  params:add{type = "option", id = "option", name = "Option", options = options, default = 1}
-  params:add{type = "option", id = "action", name = "Action", options = actions, default = 1, action=function(x) print('selection:'..x) end}
+  params:add{type = "option", id = "action", name = "Action", options = actions, default = 1, action=function(x) print('selection:'..actions[x]) end}
 end
 
 -- Render
@@ -36,25 +35,25 @@ function draw_frame()
   screen.stroke()
 end
 
-function draw_labels()
-  line_height = 8
-  -- 
+function draw_label(id,name,value)
+  local y = viewport.height - (8 * id)
   screen.level(5)
-  screen.move(5,viewport.height - (line_height * 1))
-  screen.text('number')
-  screen.move(5,viewport.height - (line_height * 2))
-  screen.text('option')
-  screen.move(5,viewport.height - (line_height * 3))
-  screen.text('action')
-  -- 
+  screen.move(5,y)
+  screen.text(name)
   screen.level(15)
-  screen.move(40,viewport.height - (line_height * 1))
-  screen.text(params:get("number"))
-  screen.move(40,viewport.height - (line_height * 2))
-  screen.text(options[params:get("option")])
-  screen.move(40,viewport.height - (line_height * 3))
-  screen.text(actions[params:get("action")])
+  screen.move(60,y)
+  if value then
+    screen.text(value)
+  end
   screen.fill()
+end
+
+function draw_labels()
+  draw_label(1,'action',actions[params:get("action")])
+  draw_label(2,'number',params:get("number"))
+  -- Library 
+  draw_label(5,'midi_output',mdh:get_output_name())
+  draw_label(4,'midi_input',mdh:get_input_name())
 end
 
 function redraw()
---
+++ lib/midi_device_helper.lua
@@ -0,0 +1,53 @@
+-- A small library to manage midi input and output parameters.
+
+local Midi_Device_Helper = { devices = {}, input = nil, output = nil }
+
+Midi_Device_Helper.init = function(self)
+  -- Get a list of midi devices
+  for id,device in pairs(midi.vports) do
+    print('Midi Device Helper','Found device: '..device.name)
+    self.devices[id] = device.name
+  end
+  -- Create Params
+  params:add{type = "option", id = "midi_output", name = "Midi Output", options = self.devices, default = 1, action=self.set_output}
+  params:add{type = "option", id = "midi_input", name = "Midi Input", options = self.devices, default = 2, action=self.set_input}
+  params:add_separator()
+  Midi_Device_Helper.set_output()
+  Midi_Device_Helper.set_input()
+end
+
+Midi_Device_Helper.get_output_name = function(self)
+  return self.devices[params:get("midi_output")]
+end
+
+Midi_Device_Helper.get_input_name = function(self)
+  return self.devices[params:get("midi_input")]
+end
+
+Midi_Device_Helper.set_output = function(x)
+  print('Midi Device Helper','Set output device: '..Midi_Device_Helper:get_output_name())
+  Midi_Device_Helper.output = midi.connect(params:get("midi_output"))
+end
+
+Midi_Device_Helper.set_input = function(x)
+  print('Midi Device Helper','Set output device: '..Midi_Device_Helper:get_input_name())
+  Midi_Device_Helper.input = midi.connect(params:get("midi_input"))
+end
+
+Midi_Device_Helper.on_input = function(self,fn)
+  if self.input == nil then print('Midi Device Helper','Missing Input Device') ; return end
+  if self.input.event then
+    self.input.event = nil
+  end
+  self.input.event = fn
+end
+
+Midi_Device_Helper.on_output = function(self,fn)
+if self.output == nil then print('Midi Device Helper','Missing Output Device') ; return end
+  if self.output.event then
+    self.output.event = nil
+  end
+  self.output.event = fn
+end
+
+return Midi_Device_Helper
